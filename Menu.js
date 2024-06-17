443c7a3d1f1a943d038d56466d97f747e32776d8
moiz-naeem <moiznaeem20@gmail.com>
2024-06-17 14:05:38 +0300

    Changed according to the MenuContext

--- Menu.js
+++ Menu.js
@@ -1,48 +1,22 @@
-import { useParams } from "react-router-dom"
-import { menuApi , DISH_ITEM_TYPE_KEY, MENU_ITEM_TYPE_KEY} from "./Constants"
-import { useEffect , useState} from "react"
+import {useContext} from "react"
 import Search from "./Search"
-import axios from "axios"
 import MenuCard from "./MenuCard"
 import NotFound from "./NotFound"
 import ShimmerUI from "./ShimmerUI"
+import MenuContext from "./MenuContext"
+
 const Menu = () => {
-    const {restaurantID} = useParams();
-    const [menuItems, setmenuItems] = useState([]);
-    const [filteredMenuItem, setFilteredMenuItem ] = useState([]);
-    const [isLoading, setLoading] = useState(true);
-    useEffect(() => {
-        const fetchMenuData = async(restaurantID) => {
-        try{
-            setLoading(true);
-            const response = await axios.get(menuApi + restaurantID);
-            const data = await response.data;
-            const menu = (data?.data?.cards.find(x=> x.groupedCard)?.
-                          groupedCard?.cardGroupMap?.REGULAR?.
-                          cards?.map(x => x.card?.card)?.
-                         filter(x=> x['@type'] === MENU_ITEM_TYPE_KEY )?.
-                        map(x=> x.itemCards).flat().map(x=> x.card?.info) || []);
-            const uniqueMenu =  menu.reduce((newMenu, item)=>{
-                 newMenu.some(x => x.id === item.id) ? null : newMenu.push(item);
-                 return newMenu 
-            }, [])
-            setLoading(false);
-            setmenuItems(uniqueMenu);
-            setFilteredMenuItem(uniqueMenu);
-        }catch(error){
-            console.error(error)
-        }
-                
-            };
-            
-        fetchMenuData(restaurantID);
-    }, [])
+
+    const {
+        menuItems,
+        setFilteredMenuItem,
+        filteredMenuItem,
+        isLoading,
+    } = useContext(MenuContext)
     
  return (
         <>
-          <div className="flex justify-center bg-orange-500 rounded-sm shadow-md">
-          <Search setFiltered={setFilteredMenuItem} raw={menuItems} type={"menu"}/>
-          </div>
+         
           <div className="flex flex-wrap justify-center ">
             {isLoading ? <ShimmerUI/> : (
                 filteredMenuItem.length ? (filteredMenuItem.map((menu) => {
